bd57d8aafe371b5e663d12d4f88241931ee873f3
Emmanuel Martinez <emmartin@microsoft.com>
2021-10-13 17:17:31 -0400

    api

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,17 +2,19 @@ import axios from "axios";
 
 import { Paste } from '../types';
 
+const { REACT_APP_API } = process.env;
+
 export const api = {
     getPublicPastes: async (): Promise<Paste[]> => {
-        const allPastesResponse: any = await axios.get('http://localhost:5000/pastes');
+        const allPastesResponse: any = await axios.get(`${REACT_APP_API}/pastes`);
         return allPastesResponse.data;
     },
     getSinglePaste: async (hash: string): Promise<Paste> => {
-        const singlePasteResponse: any = await axios.get(`http://localhost:5000/pastes/${hash}`);
+        const singlePasteResponse: any = await axios.get(`${REACT_APP_API}/${hash}`);
         return singlePasteResponse.data;
     },
     createNewPaste: async (paste: Paste): Promise<Paste> => {
-        const createdPasteResponse: any = await axios.post(`http://localhost:5000/pastes/`, {
+        const createdPasteResponse: any = await axios.post(`${REACT_APP_API}/pastes/`, {
             title: paste.title,
             text: paste.text,
             exposure: paste.exposure
@@ -20,7 +22,7 @@ export const api = {
         return createdPasteResponse.data;
     },
     editPaste: async (paste: Paste): Promise<Paste> => {
-        const editPasteResponse: any = await axios.put(`http://localhost:5000/pastes/${paste.hash}`, {
+        const editPasteResponse: any = await axios.put(`${REACT_APP_API}/pastes/${paste.hash}`, {
             title: paste.title,
             text: paste.text,
             exposure: paste.exposure
