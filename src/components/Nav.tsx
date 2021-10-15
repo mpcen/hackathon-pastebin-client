ac7c6eaf99f0fcc2414d504e4710b7bd377b7e94
emmartin <emmartin@microsoft.com>
2021-10-15 17:51:14 -0400

    sse

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -97,7 +97,7 @@ export const Nav = () => {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="/"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Your Profile
@@ -107,7 +107,7 @@ export const Nav = () => {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="/"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Settings
@@ -117,7 +117,7 @@ export const Nav = () => {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="?"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Sign out
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,60 +1,61 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import { PasteForm } from '../components/PasteForm';
-import { PasteList } from '../components/PasteList';
-import { api } from '../api';
-import { Paste } from '../types';
+import { PasteForm } from "../components/PasteForm";
+import { PasteList } from "../components/PasteList";
+import { api } from "../api";
+import { Paste } from "../types";
 
 export const HomePage = () => {
-    const [paste, setPaste] = useState<Paste>({
-        title: '',
-        text: '',
-        exposure: ''
-    });
-    const [pasteList, setPasteList] = useState<Paste[]>([]);
+  const [paste, setPaste] = useState<Paste>({
+    title: "",
+    text: "",
+    exposure: "",
+  });
+  const [pasteList, setPasteList] = useState<Paste[]>([]);
 
-    useEffect(() => {
-        const getPasteList = async () => {
-            const allPublicPastes = await api.getPublicPastes();
-            setPasteList(allPublicPastes);
-        }
+  useEffect(() => {
+    const getPastes = async () => {
+      const pasteList = await api.getPublicPastes();
+      setPasteList(pasteList);
+    };
+    getPastes();
 
-        getPasteList();
-    }, []);
+    const events = new EventSource(`${process.env.REACT_APP_API}/events`);
 
-    const onFormSubmit = async () => {
-        const response = await api.createNewPaste(paste);
+    events.onmessage = (event) => {
+      setPasteList(prevState => [...prevState, JSON.parse(event.data)]);
+    };
+  }, []);
 
-        setPaste(response);
-        setPasteList(prevState => [...prevState, response]);
-        setPaste({ title: '', text: '', exposure: '' });
-    }
+  const onFormSubmit = async () => {
+    await api.createNewPaste(paste);
+  };
 
-    return (
-        <div>
-            <header className="bg-white shadow">
-                <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <h1 className="text-3xl font-bold text-gray-900">New Paste</h1>
-                </div>
-            </header>
-            <main>
-                <div className="max-w-7xl py-4 sm:px-6 lg:px-8">
-                    <div className="flex">
-                        <div className="flex flex-6">
-                        <PasteForm 
-                            paste={paste}
-                            setPaste={setPaste}
-                            onFormSubmit={onFormSubmit}
-                        />
-                        </div>
-                        <div className="flex flex-3">
-                        <PasteList 
-                            pasteList={pasteList}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </main>
+  return (
+    <div>
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">New Paste</h1>
         </div>
-    );
-}
+      </header>
+      <main>
+        <div className="max-w-7xl py-4 sm:px-6 lg:px-8">
+          <div className="flex">
+            <div className="flex flex-6">
+              <PasteForm
+                paste={paste}
+                setPaste={setPaste}
+                onFormSubmit={onFormSubmit}
+              />
+            </div>
+            {pasteList.length > 0 ? (
+              <div className="flex flex-3">
+                <PasteList pasteList={pasteList} />
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
